ae0425414269f80af118592a4a0bdf34ffdc383e
bandeiracaio <bandeiracaio@gmail.com>
2026-02-16 02:19:23 -0300

    chore: add __init__.py for pages module

---
+++ treelab/ui/pages/__init__.py
@@ -0,0 +1,6 @@
+"""UI Pages module for TreeLab."""
+
+from treelab.ui.pages.statistics import StatisticsPage
+from treelab.ui.pages.distributions import DistributionsPage
+
+__all__ = ["StatisticsPage", "DistributionsPage"]
